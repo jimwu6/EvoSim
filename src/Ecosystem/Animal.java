e43441e5c47dc9e18303bf5870b5d341895df8c7
Justin <justinhye@gmail.com>
2019-01-09 14:19:56 -0500

    C

---
+++ src/Ecosystem/Animal.java
@@ -0,0 +1,24 @@
+package Ecosystem;
+
+import java.awt.Image;
+import java.util.*;
+
+public abstract class Animal {
+	//fields
+	public int health, size, speed, gender;
+	protected int lifespan, hunger, thirst;
+	public Image appearance;
+	protected boolean controlled;
+	protected ArrayList<Disease> disease;
+	
+	public Animal () {
+		this.health = 100;
+	}
+	
+	public abstract void drink();
+	public abstract void mate(Animal mate);
+	public abstract Animal birth();
+	public abstract void flee (Animal predator);
+	public abstract void seek (Animal animal);
+	public abstract void hurt (Integer damage);
+}
