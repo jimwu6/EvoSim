31783d56b5b28bd85c3642864ff4f860a05211b7
Justin <justinhye@gmail.com>
2019-01-12 16:33:52 -0500

    < >

--- src/GUI/Display.java
+++ src/GUI/Display.java
@@ -3,27 +3,93 @@ package GUI;
 import java.util.*;
 
 import java.awt.*;
-import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-import java.awt.Event;
+import javax.imageio.*;
+import javax.swing.*;
+import java.io.*;
 import javax.swing.event.*;
 
-public class Display {
+public class Display extends JFrame implements MouseListener{
 
+	public Display()
+	{
+		JPanel north = new JPanel(new FlowLayout());
+		settingsBtn settings = new settingsBtn();
+		settings.addMouseListener(this);
+		settings.setPreferredSize(new Dimension(111,111));
+		
+		drawArea board = new drawArea(200, 200);
+		
+		north.add(board);
+		north.add(settings);
 
+		// set window attributes
+		setContentPane(north);
+		pack();
+		setTitle("Evolution Simulation");
+		setSize(765, 790);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
     
-    class drawArea {
+    class drawArea extends JPanel {
 
-        public void paintComponent(Graphics g) {
+    	public drawArea (int width, int height)
+		{
+			// set size 
+			this.setPreferredSize(new Dimension (width, height));
+		}
 
-        }
-
+		public void paintComponent (Graphics g)
+		{			
+			Color green = new Color (0, 255, 0);
+			g.setColor(green);
+			g.fillRect(0, 0, 700, 700);
+			
+		}
     }
 
     class Advance {
 
-        Landscape landscape;
+        Territory territory;
 
     }
 
+    public static void main (String[] args) {
+    	Display window = new Display();
+		window.setVisible(true);
+		window.setResizable(false);
+    }
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ src/GUI/settingsBtn.java
@@ -0,0 +1,131 @@
+package GUI;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+import java.io.*; // allows file access
+import javax.imageio.*; // allows image loading
+ 
+public class settingsBtn extends JButton implements MouseListener {
+	Image i;
+	public boolean entered;
+	
+    public settingsBtn() {
+    	addMouseListener(this);
+    
+        try
+        {
+        	i = ImageIO.read(new File("settings.png"));
+        }
+        catch (IOException e)
+        {
+        }
+        
+        Dimension size = getPreferredSize();
+        size.width = size.height = Math.max(size.width, size.height);
+        setPreferredSize(size);
+ 
+        // This call causes the JButton not to paint the background.
+        // This allows us to paint a round background.
+        setContentAreaFilled(false);
+    }
+
+	// Paint the round background and label.
+    protected void paintComponent(Graphics g) {
+    	Color highlight = new Color(234,221,146);
+    	g.setColor(highlight);
+    	if (entered)
+    		g.fillRoundRect(0, 0, getSize().width-10, getSize().height-10, 30, 30);
+    	g.drawImage(i, 5, 5, getSize().width-20, getSize().height-20, this);
+ 
+        // This call will paint the label and the focus rectangle.
+    super.paintComponent(g);
+    }
+ 
+    // Paint the border of the button using a simple stroke.
+    protected void paintBorder(Graphics g) {
+        g.setColor(getForeground());
+       // g.drawOval(0, 0, getSize().width-1, getSize().height-1);
+    }
+ 
+    // Hit detection.
+    Shape shape;
+    public boolean contains(int x, int y) {
+        // If the button has changed size, make a new shape object.
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new Ellipse2D.Float(0, 0, getWidth(), getHeight());
+        }
+        return shape.contains(x, y);
+    }
+ 
+    @Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void mouseEntered(MouseEvent arg0) {
+		entered = true;
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		entered = false;
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+//-------------------------------------------------------------------------------------------------------------
+    // Test routine.
+    public static void main(String[] args) {
+        // Create a button with the label "Jackpot".
+        settingsBtn button = new settingsBtn();
+        button.setBounds(0, 0, 120, 120);
+ 
+        // Create a frame in which to show the button.
+        JFrame frame = new JFrame();
+        frame.getContentPane().setLayout(null);
+        frame.getContentPane().add(button);
+        
+//        frame.getContentPane().setLayout(new FlowLayout());
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+ 
+//        MouseListener mouseListener = new MouseAdapter() {
+//            public void mouseEntered( MouseEvent e )
+//            {}
+// 
+//            public void mouseExited( MouseEvent e )
+//            {}
+// 
+//            public void mouseClicked( MouseEvent e )
+//            {
+//                System.out.println( "clicked " );
+//            }
+// 
+//            public void mousePressed( MouseEvent e )
+//            {
+//                System.out.println( "pressed " );
+//            }
+// 
+//            public void mouseReleased( MouseEvent e )
+//            {
+//                System.out.println( "released " + button.entered);
+//            }
+//        };
+//        button.addMouseListener( mouseListener );
+ 
+    }
+}
