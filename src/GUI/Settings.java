fc83c11b4f739bf1200826a8b0d703ad55b4a228
Justin <justinhye@gmail.com>
2019-01-13 16:39:02 -0500

    <>

--- src/GUI/Settings.java
+++ src/GUI/Settings.java
@@ -3,19 +3,94 @@ package GUI;
 import java.util.*;
 
 import java.awt.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import java.awt.event;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.event.*;
 
-public class Settings implements MouseListener, ActionListener, ChangeListener {
+public class Settings extends JFrame implements MouseListener, ActionListener, ChangeListener {
+	//field
+	gameBtn sun, cloud, rain, reset, on, off;
+	JSlider RR, simSpeed, temp;
+	Image image = null;
 	
+	public Settings()
+	{
+		JLayeredPane content = new JLayeredPane();
+		JLabel back = new JLabel("HI");
+		 try
+	        {
+	        	image = ImageIO.read(new File("C:\\Data\\Justin\\Grade 11\\ICS\\Summative\\EvoSim\\Summative Graphics\\Menu\\png\\settings bg.png"));
+	        }
+	        catch (IOException e)
+	        {
+	        }
+		 ImageIcon icon = new ImageIcon(image);
+		// back.setIcon(icon);
+		 
+		content.add(back,(new Integer(1)));
+		
+		setContentPane(content);
+		pack();
+		setTitle("Evolution Simulation");
+		setSize(765, 790);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+
 	
-	//fields
-	JLabel speed = new JLabel("Simulation Speed");
-	JLabel weather = new JLabel("Weather");
-	JLabel temp = new JLabel("Temperature");
-	JLabel natDis = new JLabel("Natural Disasters");
-	JLabel resRates = new JLabel("Resource Rates");
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main (String[] args) {
+		Settings window = new Settings();
+		window.setVisible(true);
+		window.setResizable(false);
+	}
+	
 }
+
