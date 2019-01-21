2667a9553b9b2d76adaaadef808f96578a4b9b0b
jim.wu6@outlook.com <jimwu2002@hotmail.com>
2019-01-20 22:24:33 -0500

    update constructor

--- src/Ecosystem/Bird.java
+++ src/Ecosystem/Bird.java
@@ -1,10 +1,11 @@
 package Ecosystem;
 
-public class Bird extends Animal{
+public class Bird extends Animal {
 	
 	protected boolean flight, aquatic;
 	
-	public Bird(boolean flight, boolean aquatic) {
+	public Bird(String imageName, int size, int speed, int lifespan, String gender, boolean flight, boolean aquatic) {
+		super(imageName, size, speed, lifespan, gender);
 		this.flight = flight;
 		this.aquatic = aquatic;
 	}
